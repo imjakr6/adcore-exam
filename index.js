7a568c3c38f852ac6197a1cea60e860cc52fc932
Jake Lund <jakeandrewlund@hotmail.com>
2021-09-06 19:29:11 -0500

    Backend set up and CSV-parser

--- index.js
+++ index.js
@@ -2,31 +2,58 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var fs = require('fs');
-var csv = require('csv-parser');
-var async = require('async');
-var inputFile='tree_data.csv';
+const fs = require('fs');
+const csv = require('csv-parser');
+const async = require('async');
+const inputFile='tree_data.csv';
 
-var results=[];
+
+var nodes=[{
+    id: 0,
+    name: "root"
+}];
+
 
 function parseCSV(){
     var parser = csv({separator: '\t'})
-    .on('data', (data) => results.push(data))
+    .on('data', (data) => nodes.push(data))
     .on('error', (err) => {
         console.error(err.message)
       })
     .on('end', () => {
-        console.log(results);
+        console.log(nodes);
     });
          
     fs.createReadStream(inputFile).pipe(parser);
 }
 
 
-
-app.get('/', (req, res) => {
+app.get('/get_tree', (req, res) => {
   parseCSV();
-  res.send('Hello World!')
+  res.send(nodes);
+})
+
+app.post('/update_node', (req, res) => {
+    const id = req.body.id;
+    const name = req.body.name;
+
+    res.send(200);
+})
+
+app.delete('/delete_node', (req, res) => {
+    const id = req.body.id;
+    res.send(200);
+})
+
+app.post('/create_node', (req, res) => {
+    const parent = req.body.parent;
+    const node = req.body.node;
+    const tempID = 0;
+    res.send(tempID);
+})
+
+app.post('/export_csv', (req, res) => {
+
 })
 
 app.listen(port, () => {
